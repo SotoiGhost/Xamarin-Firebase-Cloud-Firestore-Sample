a2f29219bbd0dc5b70e99c7ba461282508e92e7a
SotoiGhost <issoto@microsoft.com>
2018-02-22 23:28:41 -0600

    Added sample that reproduces GRPCChannel dealloc

---
+++ samples/CloudFirestoreSample/CloudFirestoreSample/ViewController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using UIKit;
+using System.Threading.Tasks;
+
+using Firebase.Auth;
+using Firebase.CloudFirestore;
+
+namespace CloudFirestoreSample {
+	public partial class ViewController : UIViewController {
+		protected ViewController (IntPtr handle) : base (handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		partial void BtnTest_TouchUpInside (Foundation.NSObject sender)
+		{
+			Task.Factory.StartNew (async () => await TestFirestore ());
+		}
+
+		async Task TestFirestore ()
+		{
+			var user = await Auth.DefaultInstance.SignInAnonymouslyAsync ();
+			var dataReference = Firestore.SharedInstance.GetDocument ($"notes_app/{user.Uid}");
+			var snapshot = await dataReference.GetDocumentAsync ();
+
+			Console.WriteLine (snapshot.Exists);
+		}
+	}
+}
