2c05af65050ca9ab6bd47355b8e781ef484d66df
SotoiGhost <issoto@microsoft.com>
2018-03-05 17:16:45 -0600

    Added Xcode project so we can debug native code of Xamarin project

---
+++ externals/CloudFirestoreSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CloudFirestoreSample
+//
+//  Created by Israel Soto on 3/1/18.
+//  Copyright © 2018 Israel Soto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
